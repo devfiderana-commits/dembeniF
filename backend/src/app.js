f43094d97edbeb7ec6101719a77e8ad063be856f
devfiderana-commits <devfiderana-github@gmail.com>
2026-07-01 10:22:17 +0300

    tay2

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,7 +34,7 @@ const corsOptions = {
 
 // Middlewares
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

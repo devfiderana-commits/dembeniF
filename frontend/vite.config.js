81f0cc8e9875fe8c171ee21c4df1e4b9f21d3c4b
devfiderana-commits <devfiderana-github@gmail.com>
2026-07-02 09:06:38 +0300

    tay

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 export default defineConfig({
+  base: '/dembeniF/',
   plugins: [
     react(),
     tailwindcss(),
